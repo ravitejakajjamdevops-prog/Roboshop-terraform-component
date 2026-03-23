b6d69bab311470e34a8959ce3f888e4fcb852568
Ravi Teja Kajjam <ravitejakajjamdevops2026@gmail.com>
2026-03-23 21:12:11 +0530

    Removed terraform provider files

--- data.tf
+++ data.tf
@@ -27,9 +27,6 @@ data "aws_ssm_parameter" "public_subnet_id" {
 data "aws_ssm_parameter" "sg_id" {
     name = "/${var.project}/${var.environment}/{$var.component}_sg_id"
 }
-data "aws_ssm_parameter" "backend-lb_listener_arn" {
-    name = "/${var.project}/${var.environment}/backend-lb_listener_arn"
-}
 data "aws_ssm_parameter" "frontend-alb_listener_arn" {
     name = "/${var.project}/${var.environment}/frontend-alb_listener_arn"
 }
@@ -41,4 +38,7 @@ data "aws_ssm_parameter" "vpc_id" {
 }
 data "aws_ssm_parameter" "frontend-alb_certificate_arn" {
   name = "/Roboshop/${var.environment}/frontend-alb_certificate_arn"
+}
+data "aws_ssm_parameter" "backend-lb_listener_arn" {
+    name = "/${var.project}/${var.environment}/backend-lb_listener_arn"
 }
